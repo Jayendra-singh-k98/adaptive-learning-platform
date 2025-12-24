0dd30220cd8d09d700c5b56f297e3bed9d53d9af
JAYENDRA SINGH <jay290120@gmail.com>
2025-12-24 21:26:53 +0530

    use Suspence

--- app/features/page.js
+++ app/features/page.js
@@ -1,27 +1,15 @@
-// "use client"
-// import React from "react"
-// import Features from "../../components/Features"
-// import { useSearchParams } from "next/navigation";
-
-// export default function FeaturesPage() {
-//     const searchParams = useSearchParams();
-//   const courseId = searchParams.get("courseId");
-//   console.log("courseId from URL:", courseId);
-//     return (   
-//         <div className="pt-14">
-//             <Features courseId={courseId} />
-//         </div>
-//     )
-// }
-
-import { Suspense } from "react";
-import FeaturesClient from "./FeaturesClient";
+"use client"
+import React from "react"
+import Features from "../../components/Features"
+import { useSearchParams } from "next/navigation";
 
 export default function FeaturesPage() {
-  return (
-    <Suspense fallback={<div className="pt-14" />}>
-      <FeaturesClient />
-    </Suspense>
-  );
+    const searchParams = useSearchParams();
+  const courseId = searchParams.get("courseId");
+  console.log("courseId from URL:", courseId);
+    return (   
+        <div className="pt-14">
+            <Features courseId={courseId} />
+        </div>
+    )
 }
-
